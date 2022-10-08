d81947ae0c57713eff38dca0126477ef59cafc99
Kohei Ito <kohei.ito1012@gmail.com>
2022-10-08 17:36:34 +0900

    1st prototype

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use enum_iterator::*;
+
+mod hand;
+mod scoring;
+use hand::*;
+use scoring::*;
+
+fn main() {
+	let mut h: Hand = Hand::new_with_random_n_dice(Hand::DICE_NUM);
+	println!("Hand: {:?}", h.get_dice());
+
+	println!(" --------- 1st ---------- ");
+	for b in all::<Boxes>() {
+		let score = scoring::scoring(&b, h.get_dice());
+		println!("{}: {}", box_name(&b), score);
+	}
+
+	println!("\n --------- 2nd ---------- ");
+	let r: &[u32] = h.get_dice();
+	let r: &[u32; 3] = &[r[0], r[1], r[3]];
+	h.remove_dice(r);
+	h.add_dice(&Hand::new_with_random_n_dice(r.len()));
+	println!("Hand: {:?}", h.get_dice());
+	for b in all::<Boxes>() {
+		let score = scoring::scoring(&b, h.get_dice());
+		println!("{}: {}", box_name(&b), score);
+	}
+
+	println!("\n --------- 3nd ---------- ");
+	let r: &[u32] = h.get_dice();
+	let r: &[u32; 2] = &[r[2], r[1]];
+	h.remove_dice(r);
+	h.add_dice(&Hand::new_with_random_n_dice(r.len()));
+	println!("Hand: {:?}", h.get_dice());
+	for b in all::<Boxes>() {
+		let score = scoring::scoring(&b, h.get_dice());
+		println!("{}: {}", box_name(&b), score);
+	}
+}
---
+++ src/scoring.rs
@@ -0,0 +1,252 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use array_macro::*;
+use enum_iterator::*;
+
+use crate::hand::*;
+
+const FULL_HOUSE_SCORE: u32 = 25;
+const SMALL_STRAIGHT_SCORE: u32 = 30;
+const LARGE_STRAIGHT_SCORE: u32 = 40;
+const YAHTZEE_SCORE: u32 = 50;
+
+#[derive(PartialEq, Eq, Hash, Sequence)]
+pub enum Boxes {
+	Aces,
+	Twos,
+	Threes,
+	Fours,
+	Fives,
+	Sixes,
+	ThreeOfaAKind,
+	FourOfaAKind,
+	FullHouse,
+	SmallStraight,
+	LargeStraight,
+	Yahtzee,
+	Chance
+}
+
+pub fn box_name(b: &Boxes) -> &'static str {
+	match *b {
+		Boxes::Aces          => "Aces",
+		Boxes::Twos          => "Twos",
+		Boxes::Threes        => "Threes",
+		Boxes::Fours         => "Fours",
+		Boxes::Fives         => "Fives",
+		Boxes::Sixes         => "Sixes",
+		Boxes::ThreeOfaAKind => "Three of a kind",
+		Boxes::FourOfaAKind  => "Four of a kind",
+		Boxes::FullHouse     => "Full house",
+		Boxes::SmallStraight => "Small straight",
+		Boxes::LargeStraight => "Large straight",
+		Boxes::Yahtzee       => "Yahtzee",
+		Boxes::Chance        => "Chance",
+	}
+}
+
+pub fn scoring(b: &Boxes, dice: &[u32]) -> u32 {
+	if dice.len() != Hand::DICE_NUM {
+		panic!("A hand needs {} dice, but your hand has only {} dice.",
+			   Hand::DICE_NUM, dice.len());
+	}
+	match *b {
+		Boxes::Aces          => aces(dice),
+		Boxes::Twos          => twos(dice),
+		Boxes::Threes        => threes(dice),
+		Boxes::Fours         => fours(dice),
+		Boxes::Fives         => fives(dice),
+		Boxes::Sixes         => sixes(dice),
+		Boxes::ThreeOfaAKind => three_of_a_kind(dice),
+		Boxes::FourOfaAKind  => four_of_a_kind(dice),
+		Boxes::FullHouse     => full_house(dice),
+		Boxes::SmallStraight => small_straight(dice),
+		Boxes::LargeStraight => large_straight(dice),
+		Boxes::Yahtzee       => yahtzee(dice),
+		Boxes::Chance        => chance(dice),
+	}
+}
+
+fn aces(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 1);
+}
+
+fn twos(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 2);
+}
+
+fn threes(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 3);
+}
+
+fn fours(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 4);
+}
+
+fn fives(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 5);
+}
+
+fn sixes(dice: &[u32]) -> u32 {
+	return upper_section_scoring(dice, 6);
+}
+
+fn n_of_a_kind(dice: &[u32], n: u32) -> u32 {
+	let n: usize = n as usize;
+	for i in 0..=(Hand::DICE_NUM - n) {
+		let target: u32 = dice[i];
+		if dice.iter().filter(|&d| *d == target).count() >= n {
+			return dice.iter().sum();
+		}
+	}
+	return 0;
+}
+
+fn three_of_a_kind(dice: &[u32]) -> u32 {
+	return n_of_a_kind(dice, 3);
+}
+
+fn four_of_a_kind(dice: &[u32]) -> u32 {
+	return n_of_a_kind(dice, 4);
+}
+
+fn full_house(dice: &[u32]) -> u32 {
+	let mut sorted_dice: Vec<u32> = dice.to_vec();
+	sorted_dice.sort();
+	let init = sorted_dice[0];
+	let end = sorted_dice[Hand::DICE_NUM - 1];
+	let case0 = [init, init, init, end, end];
+	let case1 = [init, init, end, end, end];
+
+	if (init != end) && (sorted_dice == case0 || sorted_dice == case1) {
+		return FULL_HOUSE_SCORE;
+	} else {
+		return 0;
+	}
+}
+
+fn small_straight(dice: &[u32]) -> u32 {
+	let unique_dice: HashSet<u32> = dice.to_vec().into_iter().collect();
+	let mut sorted_dice: Vec<u32> = Vec::from_iter(unique_dice);
+	sorted_dice.sort();
+
+	if sorted_dice.len() < 4 {
+		return 0;
+	}
+
+	for i in 0..=(sorted_dice.len() - 4) {
+		let init: u32 = sorted_dice[i];
+		if sorted_dice[i..(i + 4)] == array![i => init + (i as u32); 4] {
+			return SMALL_STRAIGHT_SCORE;
+		}
+	}
+	return 0;
+}
+
+fn large_straight(dice: &[u32]) -> u32 {
+	let mut sorted_dice: Vec<u32> = dice.to_vec();
+	sorted_dice.sort();
+	let init = sorted_dice[0];
+
+	if sorted_dice == array![i => init + (i as u32); 5] {
+		return LARGE_STRAIGHT_SCORE;
+	} else {
+		return 0;
+	}
+}
+
+fn yahtzee(dice: &[u32]) -> u32 {
+	if dice == [dice[0]; Hand::DICE_NUM] {
+		return YAHTZEE_SCORE;
+	} else {
+		return 0;
+	}
+}
+
+fn chance(dice: &[u32]) -> u32 {
+	return dice.iter().sum();
+}
+
+fn upper_section_scoring(dice: &[u32], spots: u32) -> u32 {
+	return (dice.iter().filter(|&d| *d == spots).count() as u32) * spots;
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::scoring::*;
+
+	#[test]
+	fn upper_section_scoring_test() {
+		let dice: [u32; 5] = [1, 3, 3, 3, 6];
+		assert_eq!(scoring(&Boxes::Threes, &dice), 9);
+	}
+
+	#[test]
+	fn three_of_a_kind_test() {
+		let dice: [u32; 5] = [1, 3, 3, 3, 6];
+		assert_eq!(scoring(&Boxes::ThreeOfaAKind, &dice), 16);
+
+		let dice: [u32; 5] = [1, 3, 3, 3, 3];
+		assert_eq!(scoring(&Boxes::ThreeOfaAKind, &dice), 13);
+
+		let dice: [u32; 5] = [1, 2, 3, 3, 6];
+		assert_eq!(scoring(&Boxes::ThreeOfaAKind, &dice), 0);
+	}
+
+	#[test]
+	fn four_of_a_kind_test() {
+		let dice: [u32; 5] = [1, 3, 3, 3, 3];
+		assert_eq!(scoring(&Boxes::FourOfaAKind, &dice), 13);
+
+		let dice: [u32; 5] = [3, 3, 3, 3, 3];
+		assert_eq!(scoring(&Boxes::FourOfaAKind, &dice), 15);
+
+		let dice: [u32; 5] = [1, 2, 3, 3, 6];
+		assert_eq!(scoring(&Boxes::FourOfaAKind, &dice), 0);
+	}
+
+	#[test]
+	fn full_house_test() {
+		let dice: [u32; 5] = [4, 3, 3, 4, 3];
+		assert_eq!(scoring(&Boxes::FullHouse, &dice), FULL_HOUSE_SCORE);
+
+		let dice: [u32; 5] = [5, 5, 5, 5, 5];
+		assert_eq!(scoring(&Boxes::FullHouse, &dice), 0);
+
+		let dice: [u32; 5] = [4, 5, 1, 4, 5];
+		assert_eq!(scoring(&Boxes::FullHouse, &dice), 0);
+	}
+
+	#[test]
+	fn small_straight_test() {
+		let dice: [u32; 5] = [5, 3, 4, 4, 2];
+		assert_eq!(scoring(&Boxes::SmallStraight, &dice), SMALL_STRAIGHT_SCORE);
+
+		let dice: [u32; 5] = [2, 4, 4, 6, 4];
+		assert_eq!(scoring(&Boxes::SmallStraight, &dice), 0);
+	}
+
+	#[test]
+	fn large_straight_test() {
+		let dice: [u32; 5] = [5, 3, 1, 4, 2];
+		assert_eq!(scoring(&Boxes::LargeStraight, &dice), LARGE_STRAIGHT_SCORE);
+
+		let dice: [u32; 5] = [2, 3, 4, 1, 4];
+		assert_eq!(scoring(&Boxes::LargeStraight, &dice), 0);
+	}
+
+	#[test]
+	fn yahtzee_test() {
+		let dice: [u32; 5] = [4, 4, 4, 4, 4];
+		assert_eq!(scoring(&Boxes::Yahtzee, &dice), YAHTZEE_SCORE);
+
+		let dice: [u32; 5] = [2, 3, 5, 1, 4];
+		assert_eq!(scoring(&Boxes::Yahtzee, &dice), 0);
+	}
+
+	#[test]
+	fn chance_test() {
+		let dice: [u32; 5] = [1, 3, 5, 2, 6];
+		assert_eq!(scoring(&Boxes::Chance, &dice), 17);
+	}
+}
