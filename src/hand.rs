9df56611ff364ed7e6f890fe06a2ce66514fec11
Kohei Ito <kohei.ito1012@gmail.com>
2022-12-04 10:53:53 +0900

    Do code refactoring

--- src/hand.rs
+++ src/hand.rs
@@ -1,87 +1,85 @@
-use rand::Rng;
 use log::info;
+use rand::Rng;
 
 pub struct Hand {
-	dice: Vec<u32>,
+    dice: Vec<u32>,
 }
 
 impl Hand {
-	pub const DICE_NUM: usize = 5;
-	pub const PIPS: [u32; 6] = [1, 2, 3, 4, 5, 6];
+    pub const DICE_NUM: usize = 5;
+    pub const PIPS: [u32; 6] = [1, 2, 3, 4, 5, 6];
 
-	pub fn new_with_random_n_dice(num: usize) -> Self {
-		if num > Hand::DICE_NUM {
-			panic!("Invalud value: please input an integer between 0-5.");
-		}
-		return Hand {
-			dice: (0..num).map(|_| Hand::random_die()).collect()
-		};
-	}
+    pub fn new_with_random_n_dice(num: usize) -> Self {
+        if num > Hand::DICE_NUM {
+            panic!("Invalud value: please input an integer between 0-5.");
+        }
 
-	pub fn get_dice(&self) -> &[u32] {
-		return &self.dice;
-	}
+        Hand {
+            dice: (0..num).map(|_| Hand::random_die()).collect(),
+        }
+    }
 
-	pub fn add_dice(&mut self, elements: &Hand) {
-		if self.dice.len() + elements.dice.len() > Hand::DICE_NUM {
-			panic!("Too many dice: the number of dice is greater than five.");
-		}
-		self.dice.extend(elements.get_dice());
-	}
+    pub fn get_dice(&self) -> &[u32] {
+        &self.dice
+    }
 
-	pub fn remove_dice(&mut self, elements: &[u32]) {
-		for rm_val in elements.iter() {
-			let ret = self.dice.iter().position(|x| x == rm_val);
-			println!("value: {}", rm_val);
-			match ret {
-				Some(index) => {self.dice.swap_remove(index);},
-				None => {
-					if Hand::PIPS.contains(rm_val) {
-						info!("There is no \"{}\" to be able to remove.", rm_val);
-					} else {
-						info!("Invalid pip value: {}", rm_val);
-					}
-				},
-			};
-		}
-	}
+    pub fn add_dice(&mut self, elements: &Hand) {
+        if self.dice.len() + elements.dice.len() > Hand::DICE_NUM {
+            panic!("Too many dice: the number of dice is greater than five.");
+        }
+        self.dice.extend(elements.get_dice());
+    }
 
-	fn random_die() -> u32 {
-		return Hand::PIPS[rand::thread_rng().gen_range(0..Hand::PIPS.len())];
-	}
+    pub fn remove_dice(&mut self, elements: &[u32]) {
+        for rm_val in elements.iter() {
+            if let Some(index) = self.dice.iter().position(|x| x == rm_val) {
+                self.dice.swap_remove(index);
+            } else {
+                if Hand::PIPS.contains(rm_val) {
+                    info!("There is no \"{}\" to be able to remove.", rm_val);
+                } else {
+                    info!("Invalid pip value: {}", rm_val);
+                }
+            }
+        }
+    }
+
+    fn random_die() -> u32 {
+        Hand::PIPS[rand::thread_rng().gen_range(0..Hand::PIPS.len())]
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::hand::*;
+    use crate::hand::*;
 
-	#[test]
-	fn new_with_random_n_dice_test() {
-		for num in 0..=Hand::DICE_NUM {
-			let h = Hand::new_with_random_n_dice(num);
-			assert_eq!(h.dice.len(), num);
-		}
-	}
+    #[test]
+    fn new_with_random_n_dice_test() {
+        for num in 0..=Hand::DICE_NUM {
+            let h = Hand::new_with_random_n_dice(num);
+            assert_eq!(h.dice.len(), num);
+        }
+    }
 
-	#[test]
-	fn add_dice_test() {
-		let num: usize = 3;
-		let mut h0 = Hand::new_with_random_n_dice(num);
-		let h1 = Hand::new_with_random_n_dice(Hand::DICE_NUM - num);
-		h0.add_dice(&h1);
-		assert_eq!(h0.dice.len(), Hand::DICE_NUM);
-	}
+    #[test]
+    fn add_dice_test() {
+        let num: usize = 3;
+        let mut h0 = Hand::new_with_random_n_dice(num);
+        let h1 = Hand::new_with_random_n_dice(Hand::DICE_NUM - num);
+        h0.add_dice(&h1);
+        assert_eq!(h0.dice.len(), Hand::DICE_NUM);
+    }
 
-	#[test]
-	fn remove_dice_test() {
-		let mut h0 = Hand::new_with_random_n_dice(4);
-		let d = h0.get_dice();
-		let d = [d[0], d[2]];
-		h0.remove_dice(&d);
-		assert_eq!(h0.dice.len(), 2);
-		let d = h0.get_dice();
-		let d = [8, d[1]];
-		h0.remove_dice(&d);
-		assert_eq!(h0.dice.len(), 1);
-	}
+    #[test]
+    fn remove_dice_test() {
+        let mut h0 = Hand::new_with_random_n_dice(4);
+        let d = h0.get_dice();
+        let d = [d[0], d[2]];
+        h0.remove_dice(&d);
+        assert_eq!(h0.dice.len(), 2);
+        let d = h0.get_dice();
+        let d = [8, d[1]];
+        h0.remove_dice(&d);
+        assert_eq!(h0.dice.len(), 1);
+    }
 }
--- src/score_table.rs
+++ src/score_table.rs
@@ -1,98 +1,88 @@
-use std::collections::HashMap;
 use enum_iterator::*;
+use std::collections::HashMap;
 
 use crate::scoring::*;
 
 struct Record {
-	score: u32,
-	filled: bool
+    score: u32,
+    filled: bool,
 }
 
 impl Record {
-	fn new() -> Self {
-		return Record {
-			score: 0,
-			filled: false
-		};
-	}
+    fn new() -> Self {
+        Record {
+            score: 0,
+            filled: false,
+        }
+    }
 
-	fn fill(&mut self, score: u32) {
-		self.score = score;
-		self.filled = true;
-	}
+    fn fill(&mut self, score: u32) {
+        self.score = score;
+        self.filled = true;
+    }
 
-	fn get_score(&self) -> u32 {
-		return self.score;
-	}
+    fn get_score(&self) -> u32 {
+        self.score
+    }
 
-	fn is_filled(&self) -> bool {
-		return self.filled;
-	}
+    fn is_filled(&self) -> bool {
+        self.filled
+    }
 }
 
 pub struct ScoreTable {
-	table: HashMap<Boxes, Record>,
+    table: HashMap<Boxes, Record>,
 }
 
 impl ScoreTable {
-	pub fn new() -> Self {
-		let mut table = HashMap::new();
-		for b in all::<Boxes>() {
-			table.insert(b, Record::new());
-		}
-		return ScoreTable {
-			table: table
-		}
-	}
+    pub fn new() -> Self {
+        ScoreTable {
+            table: HashMap::from_iter(all::<Boxes>().map(|b| (b, Record::new()))),
+        }
+    }
 
-	pub fn get_score(&self, b: &Boxes) -> u32 {
-		return self.table.get(b).unwrap().get_score();
-	}
+    pub fn get_score(&self, b: &Boxes) -> u32 {
+        self.table.get(b).unwrap().get_score()
+    }
 
-	pub fn is_filled(&self, b: &Boxes) -> bool {
-		return self.table.get(b).unwrap().is_filled();
-	}
+    pub fn is_filled(&self, b: &Boxes) -> bool {
+        self.table.get(b).unwrap().is_filled()
+    }
 
-	pub fn confirm_score(&mut self, b: &Boxes, score: u32) {
-		self.table.get_mut(b).unwrap().fill(score);
-	}
+    pub fn confirm_score(&mut self, b: &Boxes, score: u32) {
+        self.table.get_mut(b).unwrap().fill(score);
+    }
 
-	pub fn remaining_boxes(&self) -> Vec<Boxes> {
-		let mut rest: Vec<Boxes> = Vec::new();
-		for b in all::<Boxes>() {
-			if !self.table.get(&b).unwrap().is_filled() {
-				rest.push(b);
-			}
-		}
-		return rest;
-	}
+    pub fn remaining_boxes(&self) -> Vec<Boxes> {
+        all::<Boxes>().filter(|b| self.is_filled(b)).collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::score_table::*;
+    use crate::score_table::*;
 
-	#[test]
-	fn test_record() {
-		let mut record = Record::new();
-		assert!(!record.is_filled());
+    #[test]
+    fn test_record() {
+        let mut record = Record::new();
+        assert!(!record.is_filled());
 
-		let score: u32 = 32;
-		record.fill(score);
-		assert_eq!(record.get_score(), score);
-		assert!(record.is_filled());
-	}
+        let score: u32 = 32;
+        record.fill(score);
+        assert_eq!(record.get_score(), score);
+        assert!(record.is_filled());
+    }
 
-	#[test]
-	fn test_score_table() {
-		let mut score_table = ScoreTable::new();
-		let b = &Boxes::Chance;
-		let score: u32 = 21;
+    #[test]
+    fn test_score_table() {
+        let mut score_table = ScoreTable::new();
+        let b = &Boxes::Chance;
+        let score: u32 = 21;
 
-		assert!(!score_table.is_filled(b));
+        assert!(!score_table.is_filled(b));
 
-		score_table.confirm_score(b, score);
-		assert!(score_table.is_filled(b));
-		assert_eq!(score_table.get_score(b), score);
-	}
+        score_table.confirm_score(b, score);
+        assert!(score_table.is_filled(b));
+        assert_eq!(score_table.get_score(b), score);
+    }
 }
